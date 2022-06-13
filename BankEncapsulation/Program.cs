cfe12855e68e3cc6282861b3106eed46e50d7e66
corey <coreymmartin1991@gmail.com>
2022-06-13 14:24:56 -0500

    first commit 2.0

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount daBank = new BankAccount();
+            bool gotDep = false;
+            double dep = 0;
+            do { 
+                Console.WriteLine("Please enter deposit amount");
+                gotDep = double.TryParse(Console.ReadLine(), out dep); 
+            } while (!gotDep);
+            daBank.Deposit(dep);
+            Console.WriteLine($"your new account balance is: {daBank.GetBalance()}");
+        }
+    }
+}
